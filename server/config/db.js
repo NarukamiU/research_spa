541445d878f51e4be0148ee6de910e8e07956592
Narukami <79750652+NarukamiU@users.noreply.github.com>
2024-12-10 21:23:52 +0900

    公開

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,22 @@ const mysql = require('mysql2');
 const util = require('util');
 require('dotenv').config(); // dotenv をロード
 
-const db = mysql.createConnection({
+/*const db = mysql.createConnection({
   host: process.env.DB_HOST,       // 環境変数を使用
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+});
+*/
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,         // Renderで設定したDB_HOST
+  port: process.env.DB_PORT,         // Renderで設定したDB_PORT
+  user: process.env.DB_USER,         // Renderで設定したDB_USER
+  password: process.env.DB_PASSWORD, // Renderで設定したDB_PASSWORD
+  database: process.env.DB_NAME,     // Renderで設定したDB_NAME
+  ssl: {
+    rejectUnauthorized: true,        // 必要に応じて設定
+  },
 });
 
 // プロミス化
--- server/config/sessionStore.js
+++ server/config/sessionStore.js
@@ -3,6 +3,7 @@
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
+/*
 const sessionStore = new MySQLStore({
   host: 'localhost',
   port: 3306,
@@ -10,5 +11,16 @@ const sessionStore = new MySQLStore({
   password: 'LV~8VNbnAYt2',       // MySQLのパスワード
   database: 'research_spa',       // 使用するデータベース
 });
+*/
+
+// セッションストアの設定
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
 
 module.exports = sessionStore;
